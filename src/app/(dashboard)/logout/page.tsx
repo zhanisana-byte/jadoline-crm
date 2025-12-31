5c408de3bca5f9e472c4c5c0635816aaf0d2b1ef
zhanisana-byte <zhanisana@gmail.com>
2025-12-31 14:45:06 +0100

    Update page.tsx

--- src/app/(dashboard)/logout/page.tsx
+++ src/app/(dashboard)/logout/page.tsx
@@ -1,7 +1,8 @@
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
+
 export default async function LogoutPage() {
-  const supabase = createClient();
+  const supabase = await createClient();
   await supabase.auth.signOut();
   redirect("/login");
 }
