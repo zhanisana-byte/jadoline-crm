890b887be39472a3a2f92721b691d7a3d0a5de78
zhanisana-byte <zhanisana@gmail.com>
2026-01-02 14:38:27 +0100

    Create layout.tsx

---
+++ src/app/profile/layout.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex">
+      <Sidebar />
+      <main className="flex-1 p-4 md:p-8">{children}</main>
+    </div>
+  );
+}
